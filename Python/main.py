4ebde63a8e2ee03c2a9a24970747681f6a78c100
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-09-19 13:18:08 +0300

    Add new solutions

--- Python/main.py
+++ Python/main.py
@@ -25,12 +25,13 @@ def longest_subarray(nums) -> int:
             if nums[left] == 0:
                 zeros -= 1
             left += 1
-        ans = max(ans, right - left + 1 - zeros)
+        # ans = max(ans, right - left + 1 - zeros)
+        ans = max(ans, right - left)
     return ans - 1 if ans == n else ans
 
 
 def summary_ranges(nums) -> str:
-    """Превратить запись в отсортерованном массиве в более короткую"""
+    """Превратить запись в отсортированном массиве в более короткую"""
     if not nums:
         return []
     ranges = []
@@ -68,9 +69,92 @@ def compress(chars) -> int:
     return ans
 
 
+class ZigzagIterator:
+    """Класс для реализации зигзагового итератора"""
+
+    def __init__(self, v1, v2):
+        self.data = [(len(v), iter(v)) for v in (v1, v2) if v]
+
+    def next(self):
+        if not self.data:
+            raise StopIteration
+
+        length, it = self.data.pop(0)
+        result = next(it)
+
+        if length > 1:
+            self.data.append((length - 1, it))
+
+        return result
+
+    def hasNext(self):
+        return bool(self.data)
+
+    def print_data(self):
+        print(self.data)
+
+
+def is_palindrome(s) -> bool:
+    """Приведение к строчным буквам и проверка на палиндром"""
+    s = [c.lower() for c in s if c.isalnum()]
+    return all(s[i] == s[~i] for i in range(len(s) // 2))
+
+
+def is_one_edit_distance(s, t):
+    """Можно ли за одно изменение сделать строки равными"""
+    m, n = len(s), len(t)
+    for i in range(min(m, n)):
+        if s[i] != t[i]:
+            if m > n:
+                return s[i + 1:] == t[i:]
+            elif m == n:
+                return s[i + 1:] == t[i + 1:]
+            else:
+                return t[i + 1:] == s[i:]
+    return abs(m - n) == 1
+
+
+def subarray_sum(nums, k):
+    """Находит количество подмассивов в текущем, которые в сумме - k"""
+    res = 0
+    prefix_sum = 0
+    d = {0: 1}
+
+    for num in nums:
+        prefix_sum += num
+        if prefix_sum - k in d:
+            res += d[prefix_sum - k]
+        if prefix_sum not in d:
+            d[prefix_sum] = 1
+        else:
+            d[prefix_sum] += 1
+
+    return res
+
+
+def move_zeros(nums) -> None:
+    """Перемещение нулей в конец массива"""
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != 0:
+            if j != i:
+                nums[j], nums[i] = nums[i], nums[j]
+            i += 1
+
+
+def group_anagrams(strs) -> list:
+    """Нахождение перестановок из набора букв"""
+    from collections import defaultdict
+    anagram_dict = defaultdict(list)
+
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        anagram_dict[sorted_word].append(word)
+    return list(anagram_dict.values())
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    if is_reflected([[1, 1], [-1, -1]]):
-        print("Yes")
-    else:
-        print("No")
+    # Для group_anagram()
+    input_strs = input().split(',')
+    print(group_anagrams(input_strs))
