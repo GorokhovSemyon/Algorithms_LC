307ef276def74c748c4c1dc8aabe87eb25604ab9
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-09-19 19:21:28 +0300

    Solve some more problems

--- Python/main.py
+++ Python/main.py
@@ -153,8 +153,165 @@ def group_anagrams(strs) -> list:
     return list(anagram_dict.values())
 
 
-# Press the green button in the gutter to run the script.
+class RandomizedSet:
+    """Вставка, удаление, получение рандомного элемента за O(1)"""
+
+    def __init__(self):
+        self.data_map = {}  # Словарь для хранения элементов и их индексов
+        self.data = []  # Список для хранения элементов
+
+    def insert(self, val: int) -> bool:
+        # Вставка элемента в RandomizedSet. Если элемент уже существует, возвращаем False.
+        if val in self.data_map:
+            return False
+
+        # Добавляем элемент в словарь, где ключ - элемент, значение - его индекс в списке
+        self.data_map[val] = len(self.data)
+
+        # Добавляем элемент в список
+        self.data.append(val)
+
+        return True
+
+    def remove(self, val: int) -> bool:
+        # Удаление элемента из RandomizedSet. Если элемент не существует, возвращаем False.
+        if val not in self.data_map:
+            return False
+
+        # Получаем последний элемент из списка
+        last_elem_in_list = self.data[-1]
+        index_of_elem_to_remove = self.data_map[val]
+
+        # Перемещаем последний элемент в место элемента, который мы хотим удалить
+        self.data_map[last_elem_in_list] = index_of_elem_to_remove
+        self.data[index_of_elem_to_remove] = last_elem_in_list
+
+        # Удаляем последний элемент из списка
+        self.data.pop()
+
+        # Удаляем элемент, который мы хотим удалить, из словаря
+        self.data_map.pop(val)
+        return True
+
+    def getRandom(self) -> int:
+        # Получение случайного элемента из списка
+        import random
+        return random.choice(self.data)
+
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.val = value
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+    """Реализация LRU"""
+
+    def __init__(self, capacity: int):
+        self.net = capacity
+        self.cache = dict()
+        self.mru = Node(-1, -1)
+        self.lru = Node(-1, -1)
+        self.mru.next = self.lru
+        self.lru.prev = self.mru
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(Node(key, value))
+        else:
+            if len(self.cache) == self.net:
+                self.remove(self.lru.prev)
+            self.insert(Node(key, value))
+
+    def remove(self, node):
+        del self.cache[node.key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def insert(self, node):
+        self.mru.next.prev = node
+        node.prev = self.mru
+        node.next = self.mru.next
+        self.mru.next = node
+
+        self.cache[node.key] = node
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return "ListNode(val=" + str(self.val) + ", next={" + str(self.next) + "})"
+
+def list_to_LL(arr):
+    if len(arr) < 1:
+        return None
+    if len(arr) == 1:
+        return ListNode(arr[0])
+    return ListNode(arr[0], next=list_to_LL(arr[1:]))
+
+
+def reverse_linked_list(head: ListNode):
+    """Разворот односвязного списка O(n)"""
+    new_list = None
+
+    while head:
+        next_node = head.next
+        head.next = new_list
+        new_list = head
+        head = next_node
+
+    return new_list
+
+
+def checkInclusion(s1, s2):
+    """Проверка есть ли в каком-то виде перестановка строки s2 в s1"""
+    from collections import Counter
+    cntr, w, match = Counter(s1), len(s1), 0
+
+    for i in range(len(s2)):
+        if s2[i] in cntr:
+            if not cntr[s2[i]]:
+                match -= 1
+            cntr[s2[i]] -= 1
+            if not cntr[s2[i]]:
+                match += 1
+
+        if i >= w and s2[i - w] in cntr:
+            if not cntr[s2[i - w]]:
+                match -= 1
+            cntr[s2[i - w]] += 1
+            if not cntr[s2[i - w]]:
+                match += 1
+
+        if match == len(cntr):
+            return True
+
+    return False
+
+
 if __name__ == '__main__':
     # Для group_anagram()
-    input_strs = input().split(',')
-    print(group_anagrams(input_strs))
+    # input_strs = input().split(',')
+    # print(group_anagrams(input_strs))
+
+    # Для reverse_linked_list()
+    tmp = list_to_LL([1, 2, 3, 4, 5])
+    print(tmp)
+    print(reverse_linked_list(tmp))
