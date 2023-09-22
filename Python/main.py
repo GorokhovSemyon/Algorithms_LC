193dd386cfd17276018eb3ab2d74870a80da0ec6
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-09-22 20:18:49 +0300

    Solve another day problem

--- Python/main.py
+++ Python/main.py
@@ -354,6 +354,16 @@ def min_usb_cost(n, m, c2, c5):
     return total_cost
 
 
+def isSubsequence(self, s: str, t: str) -> bool:
+    """Есть ли набор символов из t в s"""
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
+
+
 
 if __name__ == '__main__':
     # Для group_anagram()
