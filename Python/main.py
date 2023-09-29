afed3254d7ca4c60fe86834f4088339bf6b9e482
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-09-29 10:59:36 +0300

    Solve LC896

--- Python/main.py
+++ Python/main.py
@@ -451,6 +451,27 @@ def sortArrayByParity(nums) -> list:
     return [x for x in nums if x % 2 == 0] + [x for x in nums if x % 2 == 1]
 
 
+def isMonotonic(nums) -> bool:
+    """Проверка монотонности последовательности"""
+    if len(nums) < 2:
+        return True
+
+    direction = 0  # 0 means unknown, 1 means increasing, -1 means decreasing
+
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i - 1]:  # increasing
+            if direction == 0:
+                direction = 1
+            elif direction == -1:
+                return False
+        elif nums[i] < nums[i - 1]:  # decreasing
+            if direction == 0:
+                direction = -1
+            elif direction == 1:
+                return False
+
+    return True
+
 
 if __name__ == '__main__':
     # Для group_anagram()
