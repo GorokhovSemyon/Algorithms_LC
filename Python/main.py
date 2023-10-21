89ff1174a840d26d3594c4f047226481ec66c219
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-10-21 11:46:46 +0300

    Solve another day problem

--- Python/main.py
+++ Python/main.py
@@ -838,6 +838,27 @@ def backspace_сompare(s: str, t: str) -> bool:
 
     return True  # Если не вышли ни на одном return, то строки эквивалентны
 
+def constrainedSubsetSum(nums, k) -> int:
+    """
+    Вернуть максимальную сумму подсписка длиной не менее k
+    LC1425
+    :param nums:
+    :param k:
+    :return: максимальная сумма
+    """
+    from collections import deque
+    dq = deque()
+    for i in range(len(nums)):
+        nums[i] += nums[dq[0]] if dq else 0
+
+        while dq and (i - dq[0] >= k or nums[i] >= nums[dq[-1]]):
+            dq.pop() if nums[i] >= nums[dq[-1]] else dq.popleft()
+
+        if nums[i] > 0:
+            dq.append(i)
+
+    return max(nums)
+
 
 if __name__ == '__main__':
     # Для group_anagram()
