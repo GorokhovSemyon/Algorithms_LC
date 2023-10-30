bbc5fed2d25b7aabf5176581dfec9fb54ccd5fc1
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-10-30 17:10:26 +0300

    Minor style update

--- Python/main.py
+++ Python/main.py
@@ -1096,7 +1096,7 @@ def sort_by_bits(arr):
         Сортировка по количеству единиц в бинарном представлении
         LC1356
         :param arr: список чисел
-        :return: custom_sort(arr)
+        :return: spec_sort(arr)
     """
     def binary_sort_key(num):
         cnt_of_positive_bits = bin(num).count('1')
