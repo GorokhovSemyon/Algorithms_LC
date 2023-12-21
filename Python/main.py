73ff0ba4a4d4efa7bfd79fd944bed27765d1fa2a
GorokhovSemyon <SamGorokhov@yandex.ru>
2023-12-21 22:10:04 +0300

    Solve LC1637

--- Python/main.py
+++ Python/main.py
@@ -1386,6 +1386,17 @@ def image_smoother(img):
         res.append(temp)
     return res
 
+def max_widthOfVerticalArea(points) -> int:
+    points.sort(key=lambda x: x[0])
+
+    max_width = 0
+
+    for i in range(1, len(points)):
+        width = points[i][0] - points[i - 1][0]
+        max_width = max(max_width, width)
+
+    return max_width
+
 
 if __name__ == '__main__':
     # Для group_anagram()
