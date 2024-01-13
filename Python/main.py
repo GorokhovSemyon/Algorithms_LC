9bd2328e4a47be1b6f63205e6a34367927cadf91
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-01-13 16:23:00 +0300

    Solve LC1347

--- Python/main.py
+++ Python/main.py
@@ -1511,6 +1511,23 @@ def min_operations(nums) -> int:
     return cnt
 
 
+def minSteps(s: str, t: str) -> int:
+    count_s = [0] * 26
+    count_t = [0] * 26
+
+    for char in s:
+        count_s[ord(char) - ord('a')] += 1
+
+    for char in t:
+        count_t[ord(char) - ord('a')] += 1
+
+    steps = 0
+    for i in range(26):
+        steps += abs(count_s[i] - count_t[i])
+
+    return steps // 2
+
+
 if __name__ == '__main__':
     # Для group_anagram()
     # input_strs = input().split(',')
