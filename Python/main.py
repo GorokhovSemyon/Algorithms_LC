c5acf55f5251d561e2d7935b2f9ccf372064214e
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-01-31 23:01:55 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -427,6 +427,7 @@ def is_subsequence(s: str, t: str) -> bool:
 def longest_str_chain(words):
     """
         Поиск самой длинной цепочки (LC1048)
+        :param words: список слов [List[str]]
     """
     dp = {}
     for w in sorted(words, key=len):
