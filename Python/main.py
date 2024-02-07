2d36761ca2901d33c6bfc75ad803af432244f081
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-02-07 20:58:22 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -2,7 +2,7 @@ def is_reflected(points) -> bool:
     """
         Найти линию, паралельную y, которая отражает точки (O(n))
         :param points: входной массив, отражающий точки линии [List[int]]
-        :return: является ли отражённой [bool]
+        :return: является отражённой [bool]
     """
     min_x, max_x = float('inf'), float('-inf')
     point_set = set()
