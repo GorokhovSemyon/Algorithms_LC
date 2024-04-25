3a1bf31f9bde5d04b3f9bc17ec45f74bb33c9273
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-04-25 23:30:16 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1182,7 +1182,7 @@ def poor_pigs(buckets: int, minutesToDie: int, minutesToTest: int) -> int:
     """
         Определить отравленное ведро
         LC458
-        :param buckets: количество вёдер
+        :param buckets: количество вёдер [int]
         :param minutesToDie: время на тест...
         :param minutesToTest: Время для определения
         :return: количество требуемых свинок [int]
