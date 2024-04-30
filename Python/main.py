20993e04031879ed15debe1c437bfe7463a5c139
Горохов Семён <92113292+GorokhovSemyon@users.noreply.github.com>
2024-04-30 22:22:05 +0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1395,7 +1395,7 @@ def max_product_difference(nums):
     """
         Найти наибольшую разницу между произведениями
         LC1913
-        :param nums: входной список List[int]
+        :param nums: входной список [List[int]]
         :return: наибольшая разница [int]
     """
     largest, secondLargest = 0, 0
